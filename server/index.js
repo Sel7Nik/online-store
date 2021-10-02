1964b3b7d780f54639218251d89eb7d099644994
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-10-02 20:33:53 +0300

    route

--- server/index.js
+++ server/index.js
@@ -3,8 +3,17 @@ const express = require('express');
 const sequelize = require('./db');
 const models = require('./models/models');
 const PORT = process.env.PORT || 5000;
+const cors = require('cors');
+const router = require('./routes/index.js');
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/api', router);
+// проверка работоспособности сервера и БД
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'WORKING' });
+// });
 
 const start = async () => {
   try {
